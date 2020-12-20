a6542451de5fbbc28b3daf0f981e93516aee2766
Med Ahmed Kilani <70693716+Med-Ahmed-Kilani@users.noreply.github.com>
2020-12-20 16:56:41 +0100

    Withdraw Methods :D

--- app/src/main/java/com/example/firststepv01/Profile.java
+++ app/src/main/java/com/example/firststepv01/Profile.java
@@ -7,9 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Profile extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
 
+public class Profile extends AppCompatActivity implements RewardedVideoAdListener {
+
+    private RewardedVideoAd mRewardedVideoAd;
     Button work, withdraw;
     TextView username;
 
@@ -17,6 +25,10 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713");
+
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.setRewardedVideoAdListener((RewardedVideoAdListener) this);
 
         work = findViewById(R.id.work_btn);
         withdraw = findViewById(R.id.withdraw_btn);
@@ -43,7 +55,59 @@ public class Profile extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), Withdraw.class);
                 startActivity(i);
+                loadRewardedVideoAd();
+                if (mRewardedVideoAd.isLoaded()) {
+                    mRewardedVideoAd.show();
+                }
             }
         });
+    }
+
+    private void loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+    }
+
+
+    public void onRewarded(RewardItem reward) {
+        Toast.makeText(this, "onRewarded! currency: " + reward.getType() + "  amount: " +
+                reward.getAmount(), Toast.LENGTH_SHORT).show();
+        // Reward the user.
+    }
+
+
+    public void onRewardedVideoAdLeftApplication() {
+        Toast.makeText(this, "onRewardedVideoAdLeftApplication",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRewardedVideoAdClosed() {
+        Toast.makeText(this, "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewarded(com.google.android.gms.ads.reward.RewardItem rewardItem) {
+        Toast.makeText(this, "onRewarded! currency: " + "  amount: ", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void onRewardedVideoAdFailedToLoad(int errorCode) {
+        Toast.makeText(this, "onRewardedVideoAdFailedToLoad", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRewardedVideoAdLoaded() {
+        Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRewardedVideoAdOpened() {
+        Toast.makeText(this, "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRewardedVideoStarted() {
+        Toast.makeText(this, "onRewardedVideoStarted", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRewardedVideoCompleted() {
+        Toast.makeText(this, "onRewardedVideoCompleted", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/firststepv01/Withdraw.java
+++ app/src/main/java/com/example/firststepv01/Withdraw.java
@@ -3,17 +3,69 @@ package com.example.firststepv01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
-public class Withdraw extends AppCompatActivity {
+public class Withdraw extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    TextView d17;
+    Button submitButtonWithdraw;
+    EditText phoneNumberd17, amountToWithdraw, accountNumberCED;
+    Spinner spinner;
+    LinearLayout d17Layout, cbLayout, cedLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_withdraw);
-        
 
+        submitButtonWithdraw = findViewById(R.id.submitButtonWithdraw);
+        phoneNumberd17 = findViewById(R.id.phoneNumberD17);
+        amountToWithdraw = findViewById(R.id.amountToWithdraw);
+        accountNumberCED = findViewById(R.id.accountNumberCED);
+        spinner = findViewById(R.id.spinner);
+        d17Layout = findViewById(R.id.D17LayoutPayment);
+        cbLayout = findViewById(R.id.cbLayoutPayment);
+        cedLayout = findViewById(R.id.carteEdinarLayoutPayment);
+
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Withdraw_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (parent.getItemAtPosition(position).equals("D17")){
+            disable();
+            amountToWithdraw.setVisibility(View.VISIBLE);
+            d17Layout.setVisibility(View.VISIBLE);
+            submitButtonWithdraw.setVisibility(View.VISIBLE);
+        } else if (parent.getItemAtPosition(position).equals("Carte Bancaire")){
+            disable();
+            cbLayout.setVisibility(View.VISIBLE);
+        } else {
+            disable();
+            amountToWithdraw.setVisibility(View.VISIBLE);
+            cedLayout.setVisibility(View.VISIBLE);
+            submitButtonWithdraw.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    public void disable() {
+        submitButtonWithdraw.setVisibility(View.GONE);
+        amountToWithdraw.setVisibility(View.GONE);
+        d17Layout.setVisibility(View.GONE);
+        cbLayout.setVisibility(View.GONE);
+        cedLayout.setVisibility(View.GONE);
     }
 }
